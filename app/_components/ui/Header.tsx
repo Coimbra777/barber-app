d03c3d20d052e9c9d7c1971a10d14d1cdd28f191
Coimbra777 <gabrielfelipe.coimbra@gmail.com>
2024-03-19 13:44:40 -0300

    feat: mensagem de boas-vindas

--- app/_components/ui/Header.tsx
+++ app/_components/ui/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <Card>
       <CardContent className='p-5 justify-between itens-center flex flex-row'>
-      <Image src="/img/logo.png" alt="FSW Barber" height={22} width={120}>
+      <Image src="/img/logo.png" alt="FSW Barber" height={22} width={140}>
       </Image>
       <Button variant="outline" size="icon">
         <MenuIcon size={16}/>
