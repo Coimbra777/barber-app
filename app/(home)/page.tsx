fda875473953963a36dea0e780bfd2c77befea9c
Coimbra777 <gabrielfelipe.coimbra@gmail.com>
2024-03-19 15:01:41 -0300

    feat: pagina incial dos agendamentos

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { format } from "date-fns";
 import Header from "../_components/Header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/Search";
+import BookingItem from "../_components/Bookins-item";
 
 export default function Home() {
   return (
@@ -19,6 +20,10 @@ export default function Home() {
 
       <div className="px-5 mt-6">
         <Search />
+      </div>
+      <div className="px-5 mt-6">
+        <h2 className="text-xs uppercase text-gray-500 font-bold mb-3">Agendamentos</h2>
+        <BookingItem />
       </div>
     </div>
   );
