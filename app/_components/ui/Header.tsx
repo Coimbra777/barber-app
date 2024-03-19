65a9eaeface08580dc5c190ab829c1062b0d8371
Coimbra777 <gabrielfelipe.coimbra@gmail.com>
2024-03-19 12:54:59 -0300

    feat: adição header da aplicação

---
+++ app/_components/ui/Header.tsx
@@ -0,0 +1,19 @@
+import { MenuIcon } from 'lucide-react';
+import { Button } from './button';
+import {Card, CardContent} from './card'
+import Image from 'next/image'
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className='p-5 justify-between itens-center flex flex-row'>
+      <Image src="/img/logo.png" alt="FSW Barber" height={22} width={120}>
+      </Image>
+      <Button variant="outline" size="icon">
+        <MenuIcon size={16}/>
+      </Button>
+      </CardContent>
+    </Card>
+  )
+}
+ 
+export default Header;
